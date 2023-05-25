dc9c1befb9adb0e32f4bb4fc09fcca20d54aceec
Chandralekha.S <Chandralekha.S@CHANDRALEK-LT>
2023-05-25 11:07:18 +0530

    Add project files.

---
+++ sampleAssignments/javaScript.js
@@ -0,0 +1,5 @@
+function displayName () {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    alert("Full name : "+firstname+" "+lastname);
+}
