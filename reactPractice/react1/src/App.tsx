613d109b7c7e24b8c4cff695a010c52b0a32f1cf
Chandralekha S <Chandralekha.S@trivium-esolutions.com>
2023-05-25 13:36:55 +0530

    New project added

---
+++ reactPractice/react1/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Message from './Component/Message'
+
+function App() {
+  return <Message></Message>
+}
+
+export default App
---
+++ reactPractice/react1/src/Component/Message.tsx
@@ -0,0 +1,8 @@
+function Message(){
+    const name = "chandu";
+    if (name)
+        return <h1>Hii {name}</h1>
+    return <h1>Hii</h1>    
+}
+
+export default Message
