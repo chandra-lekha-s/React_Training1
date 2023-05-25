c30c223ab95ec0affeeef29308658421d4e64383
Chandralekha S <Chandralekha.S@trivium-esolutions.com>
2023-05-25 11:38:30 +0530

    updated index.html and js file

--- sampleAssignments/javaScript.js
+++ sampleAssignments/javaScript.js
@@ -1,5 +1,3 @@
 function displayName () {
-    var firstname = document.getElementById("firstname").value;
-    var lastname = document.getElementById("lastname").value;
-    alert("Full name : "+firstname+" "+lastname);
+    alert("Full name : "+document.getElementById("firstname").value+" "+document.getElementById("lastname").value);
 }
